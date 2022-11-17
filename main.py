4ff13f59894688db7c7f4d2908c162af7bf4744d
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-11-16 21:03:42 -0600

    update to django app

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from typing import Union
+from datetime import datetime, timedelta
+from typing import Union, List
 
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-from django.http import JsonResponse
+from fastapi import Depends, FastAPI, HTTPException, status, Security
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, SecurityScopes
+
+import jwt
+from pydantic import BaseModel, ValidationError
+from passlib.context import CryptContext
+
+from django.http import JsonResponse, HttpResponse
+import variables
 
 import requests
+
+SECRET_KEY='91d7569c731d4d8b690184abd5f54472be51c916a7332a35c02f3a011a3dcdd8'
+ALGORITHM='HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES= 30
 
 fake_users_db = {
     "johndoe": {
@@ -24,15 +34,13 @@ fake_users_db = {
     },
 }
 
-app = FastAPI()
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
-
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+    scopes: List[str] = []
 
 class User(BaseModel):
     username: str
@@ -44,6 +52,22 @@ class User(BaseModel):
 class UserInDB(User):
     hashed_password: str
 
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="token",
+    scopes={"me": "Read information about the current user.", "items": "Read items."},
+)
+
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 def get_user(db, username: str):
     if username in db:
@@ -51,35 +75,81 @@ def get_user(db, username: str):
         return UserInDB(**user_dict)
 
 
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
-    return user
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = fake_decode_token(token)
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
     return user
 
 
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(
+    security_scopes: SecurityScopes, token: str = Depends(oauth2_scheme)
+):
+    if security_scopes.scopes:
+        authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
+    else:
+        authenticate_value = "Bearer"
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": authenticate_value},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_scopes = payload.get("scopes", [])
+        token_data = TokenData(scopes=token_scopes, username=username)
+    except (JWTError, ValidationError):
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    for scope in security_scopes.scopes:
+        if scope not in token_data.scopes:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Not enough permissions",
+                headers={"WWW-Authenticate": authenticate_value},
+            )
+    return user
+
+
+async def get_current_active_user(
+    current_user: User = Security(get_current_user, scopes=["me"])
+):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
-@app.post("/token")
-async def login2():
-    response = requests.get("https://launchpad.37signals.com/authorization/token")
-    print(response)
-    return "response"
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username, "scopes": form_data.scopes},
+        expires_delta=access_token_expires,
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+# @app.post("/token")
 # async def login(form_data: OAuth2PasswordRequestForm = Depends()):
 #     user_dict = fake_users_db.get(form_data.username)
 #     if not user_dict:
@@ -91,23 +161,53 @@ async def login2():
 
 #     return {"access_token": user.username, "token_type": "bearer"}
 
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
 
-@app.get("/users/me")
-async def users():
-    # # Get a copy of the default headers that requests would use
-    # headers = requests.utils.default_headers()
 
-    # # Update the headers with your custom ones
-    # # You don't have to worry about case-sensitivity with
-    # # the dictionary keys, because default_headers uses a custom
-    # # CaseInsensitiveDict implementation within requests' source code.
-    # headers.update(
-    #     {
-    #         'User-Agent': 'My User Agent 1.0',
-    #     }
-    # )
-    response = requests.get("https://launchpad.37signals.com/authorization/new?type=web_server&client_id=fdfcd31459e0e7491296ea11a0dd53ab984e3417&client_secret=3be318f63a3047d55fcb84d22959a6c6f77e62f2&redirect_uri=http://localhost:8000/token")
-    print(JsonResponse(response))
-    return "response"
-# async def read_users_me(current_user: User = Depends(get_current_active_user)):
-#     return current_user
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: User = Security(get_current_active_user, scopes=["items"])
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+
+
+@app.get("/status/")
+async def read_system_status(current_user: User = Depends(get_current_user)):
+    return {"status": "ok"}
+
+
+
+
+# @app.get("/users/me")
+# async def users():
+#     # # Get a copy of the default headers that requests would use
+#     # headers = requests.utils.default_headers()
+
+#     # # Update the headers with your custom ones
+#     # # You don't have to worry about case-sensitivity with
+#     # # the dictionary keys, because default_headers uses a custom
+#     # # CaseInsensitiveDict implementation within requests' source code.
+#     # headers.update(
+#     #     {
+#     #         'User-Agent': 'My User Agent 1.0',
+#     #     }
+#     # )
+#     response = requests.get("https://launchpad.37signals.com/authorization/new?type=web_server&client_id=fdfcd31459e0e7491296ea11a0dd53ab984e3417&client_secret=3be318f63a3047d55fcb84d22959a6c6f77e62f2&redirect_uri=http://localhost:8000/token")
+#     print(JsonResponse(response))
+#     return "response"
+# # async def read_users_me(current_user: User = Depends(get_current_active_user)):
+# #     return current_user
+
+
+
+# @app.post("/token")
+# async def login2(form_data: OAuth2PasswordRequestForm = Depends()): #This form data depends on the oauth2 password req form
+#     response = requests.get("https://launchpad.37signals.com/authorization/token")
+#     print(response)
+#     return {"access_token": form_data.username + 'token'}
+
+# @app.get('/')
+# async def index(token: str = Depends(oauth2_scheme())): #call oauth2scheme- is there a token?
+#     return {'the_token' : token}
